5fd22892b0beb6ea2477f79c88fe25289af9754c
Kutay Can Kaynak <56002368+kutagorn@users.noreply.github.com>
2024-05-02 22:58:22 +0100

    Lint

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -9,16 +9,16 @@ const ControlContainer = styled.div`
 `;
 
 const Label = styled.label`
-display: block;
-margin-bottom: 0.5rem;
-font-size: 0.75rem;
-font-weight: 700;
-letter-spacing: 0.1em;
-text-transform: uppercase;
-color: ${(props) => props.invalid ? '#f87171;' : '#6b7280' };
-`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  font-weight: 700;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  color: ${(props) => (props.invalid ? "#f87171;" : "#6b7280")};
+`;
 const Input = styled.input`
-width: 100%;
+  width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
   background-color: #d1d5db;
@@ -26,7 +26,7 @@ width: 100%;
   border: 1px solid transparent;
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`
+`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -52,9 +52,7 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlContainer>
         <p className="paragraph">
-          <Label invalid={emailNotValid} >
-            Email
-          </Label>
+          <Label invalid={emailNotValid}>Email</Label>
           <Input
             type="email"
             className={emailNotValid ? "invalid" : undefined}
